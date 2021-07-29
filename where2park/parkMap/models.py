ae657ec38e900fd5c423b0cce36b2a5d2e1daa4b
Lenore Chen <lenore@profitablesoftware.com>
2021-07-29 11:14:09 -0700

    upload meters to database

--- where2park/parkMap/models.py
+++ where2park/parkMap/models.py
@@ -4,26 +4,26 @@ from django.utils import timezone
 
 # Create your models here.
 class Meter(models.Model):
-    rate_weekday_9A_6P = models.FloatField('R_MF_9A_6P')
-    rate_weekday_6P_10P = models.FloatField('R_MF_6P_10')
-    rate_sat_9A_6P = models.FloatField('R_SA_9A_6P')
-    rate_sat_6P_10P = models.FloatField('R_SA_6P_10')
-    rate_sun_9A_6P = models.FloatField('R_SU_9A_6P')
-    rate_sun_6P_10P = models.FloatField('R_SU_6P_10')
+    rate_weekday_9A_6P = models.FloatField('R_MF_9A_6P', null = True)
+    rate_weekday_6P_10P = models.FloatField('R_MF_6P_10', null = True)
+    rate_sat_9A_6P = models.FloatField('R_SA_9A_6P', null = True)
+    rate_sat_6P_10P = models.FloatField('R_SA_6P_10', null = True)
+    rate_sun_9A_6P = models.FloatField('R_SU_9A_6P', null = True)
+    rate_sun_6P_10P = models.FloatField('R_SU_6P_10', null = True)
 
-    time_weekday_9A_6P = models.FloatField('T_MF_9A_6P')
-    time_weekday_6P_10P = models.FloatField('T_MF_6P_10')
-    time_sat_9A_6P = models.FloatField('T_SA_9A_6P')
-    time_sat_6P_10P = models.FloatField('T_SA_6P_10')
-    time_sun_9A_6P = models.FloatField('T_SU_9A_6P')
-    time_sun_6P_10P = models.FloatField('T_SU_6P_10')
+    time_weekday_9A_6P = models.FloatField('T_MF_9A_6P', null = True)
+    time_weekday_6P_10P = models.FloatField('T_MF_6P_10', null = True)
+    time_sat_9A_6P = models.FloatField('T_SA_9A_6P', null = True)
+    time_sat_6P_10P = models.FloatField('T_SA_6P_10', null = True)
+    time_sun_9A_6P = models.FloatField('T_SU_9A_6P', null = True)
+    time_sun_6P_10P = models.FloatField('T_SU_6P_10', null = True)
 
-    credit_card = models.BooleanField('CREDITCARD')
-    pay_phone = models.IntegerField('PAY_PHONE')
+    credit_card = models.BooleanField('CREDITCARD', null = True)
+    pay_phone = models.CharField('PAY_PHONE', max_length = 10)
     lat = models.FloatField('Latitude')
     long = models.FloatField('Longitude')
     location = models.CharField('Geo Local Area', max_length = 140)
-    meter_id = models.IntegerField('METERID', primary_key= True)
+    meter_id = models.CharField('METERID', max_length = 10, primary_key= True)
 
     def __str__(self):
         return "{} MeterID: {}".format(self.location, self.meter_id)
--- where2park/parkMap/views.py
+++ where2park/parkMap/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at parkMap index.")
+# def index(request):
+#     return HttpResponse("Hello, world. You're at parkMap index.")
 
+def index(request):
+    context = {'hi'}
+    return render(request, 'parkMap/index.html')
 
 def detail(request, meter_id):
     return HttpResponse("You're looking at meter %s." % meter_id)
