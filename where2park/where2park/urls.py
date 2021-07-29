03555bb2074b5ee89637ebfb7a1f04f160ff1904
Lenore Chen <lenore@profitablesoftware.com>
2021-07-29 11:14:09 -0700

    upload meters to database

--- where2park/where2park/urls.py
+++ where2park/where2park/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+
+
 urlpatterns = [
     path('parkmap/', include('parkMap.urls')),
     path('admin/', admin.site.urls),
