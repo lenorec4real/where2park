de038bde632e445532765022eae3d871f991506e
Lenore Chen <lenore@profitablesoftware.com>
2021-08-31 23:30:41 -0700

    change center location by clicking

--- where2park/parkMap/urls.py
+++ where2park/parkMap/urls.py
@@ -4,8 +4,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # path('test', views.test, name = 'test'),
+    path('test/<str:lat>/<str:lon>/<str:threshold>', views.getNearestMeters, name = 'getMeters'), # e.g.: http://localhost:8000/parkmap/distance/49.2827/-123.1207/10
     path('', views.index, name='index'),
     path('<str:meter_id>/', views.detail, name='detail'),
     path('<str:meter_id>/rate', views.getMeterRate, name = 'getRate'),
-    path('distance/<str:lat>/<str:lon>/<str:threshold>', views.getNearestMeters, name = 'getMeters')
+    path('distance/<str:lat>/<str:lon>/<str:threshold>', views.getNearestMeters, name = 'getMeters'), # e.g.: http://localhost:8000/parkmap/distance/49.2827/-123.1207/10
+    path('update-marker/<str:lat>/<str:lon>/<str:threshold>', views.getNearestMetersJSON, name = 'updateMarker'),
 ]
--- where2park/parkMap/views.py
+++ where2park/parkMap/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
 from .models import Meter
 from datetime import datetime
 import math
@@ -7,6 +9,8 @@ import json
 
 METER_NUM_DISPLAY = 40
 
+def test(request):
+    return render(request, 'parkMap/test.html')
 
 def index(request):
     context = {'hi'}
@@ -43,7 +47,7 @@ def getMeterRate(request, meter_id):
 
 '''
     Get meters that are within *threshold(km)* distance to the location (lat, long),
-    up to 20 meters.
+    up to *METER_NUM_DISPLAY* meters.
     Filter by the location first.
 '''
 def getNearestMeters(request, lat, lon, threshold):
@@ -59,25 +63,46 @@ def getNearestMeters(request, lat, lon, threshold):
             distance[meter.meter_id] = dist
 
     meterList = sorted(distance.items(), key=lambda x: x[1])
-
     f = open('./parkMap/token.txt', 'r')
     token = f.read()
     f.close()
     meterList = [Meter.objects.filter(meter_id = x[0])[0] for x in meterList[:METER_NUM_DISPLAY]]
-    rate = [m.getCurrentRate() for m in meterList]
     result =  {
         'closest_meters':meterList, 
         'mapbox_access_token': token,
         'center_lat': lat,
         'center_long': lon,
-        'rate': rate
-        }
-    return render(request, 'parkMap/meters.html',result)
+        } 
+    return render(request, 'parkMap/test.html',result)
+    # return render(request, 'parkMap/meters.html',result)
 
+def getNearestMetersJSON(request, lat, lon, threshold):
+    meters = Meter.objects.all()
+    lat = float(lat)
+    lon = float(lon)
+    threshold = float(threshold)
+    distance = dict()
+    result = dict()
+    for meter in meters:
+        dist = getDistance(lat, lon, meter.lat, meter.long)
+        if (dist <= threshold):
+            distance[meter.meter_id] = dist
 
+    meterList = sorted(distance.items(), key=lambda x: x[1])
+    f = open('./parkMap/token.txt', 'r')
+    token = f.read()
+    f.close()
+    meterList = [model_to_dict(Meter.objects.filter(meter_id = x[0])[0]) for x in meterList[:METER_NUM_DISPLAY]]
+    result =  {
+        'closest_meters': meterList, 
+        'mapbox_access_token': token,
+        'center_lat': lat,
+        'center_long': lon,
+        } 
+    return JsonResponse(json.dumps(result), safe=False)
 
 '''
-    Compute ecludian distance between location1 and location2
+    Compute ecludian distance between location1(lat1, lon1) and location2(lat2, lon2)
 '''
 def getDistance(lat1, lon1, lat2, lon2):
     R = 6373.0 #radius of the Earth
@@ -85,12 +110,9 @@ def getDistance(lat1, lon1, lat2, lon2):
     lon1 = math.radians(lon1)
     lat2 = math.radians(lat2)
     lon2 = math.radians(lon2)
-
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-
     a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     distance = R * c
-
     return distance
