dd1398b6494897641df3d130fc2690cc80df93ea
Lenore Chen <lenore@profitablesoftware.com>
2021-09-18 18:53:30 -0700

    clean up

--- where2park/where2park/settings.py
+++ where2park/where2park/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qi1!7&x*na68s@h8wc5^65)lnpebot+crsw+dh9#eyl-^#fm2('
-
+# SECRET_KEY = 'django-insecure-qi1!7&x*na68s@h8wc5^65)lnpebot+crsw+dh9#eyl-^#fm2('
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
